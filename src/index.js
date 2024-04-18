107f7b362acef91d10150f11cdfaa64c8d3ecb89
Ajay Sinha <ajaysinha.enest@gmail.com>
2024-04-18 19:08:06 +0530

    edited app file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Error connecting to the database !!!", err));
 
 /*
 import express from "express";
